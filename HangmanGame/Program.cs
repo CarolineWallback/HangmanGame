6bdfa33b2bb00e354d9c8910f0d8c0af269e3b0a
lexicon <lexicon@DESKTOP-86DJO9S>
2022-09-26 12:00:09 +0200

    updated Program.cs to better ignore case sensitivity

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -9,16 +9,14 @@ namespace HangmanGame
     internal class Program
     {
         static Random random = new Random();
-        static string[] words = new string[10] { "bubblegum", "kiwifruit", "keyhole", "espionage", "microwave", "peekaboo", "nightclub", "unknown", "jukebox", "mystery" };
-        static int guesses = 10;
-
+        static string[] words = new string[10] { "Bubblegum", "Kiwifruit", "Keyhole", "Espionage", "Microwave", "Peekaboo", "Nightclub", "Unknown", "Jukebox", "Mystery" };
+       
         static void Main(string[] args)
         {
             string chosenWord = words[random.Next(0, words.Length)];
             char [] word = new char[chosenWord.Length];
             StringBuilder guessedLetters = new StringBuilder();
-
-            
+            int guesses = 10;
 
             for (int i = 0; i < word.Length; i++)
             {
@@ -39,14 +37,14 @@ namespace HangmanGame
                 string letter = "";
                 while (!Regex.IsMatch(letter, "[A-Za-z]"))
                 {
-                    letter = (Console.ReadLine().ToLower());
-                    if (!Regex.IsMatch(letter, "[a-z]"))
+                    letter = (Console.ReadLine());
+                    if (!Regex.IsMatch(letter, "[A-Za-z]"))
                         Console.WriteLine("You can only use letters A-Z");
                 }
 
                 if(letter.Length > 1)
                 {
-                    if(letter == chosenWord)
+                    if(letter.Equals(chosenWord, StringComparison.OrdinalIgnoreCase))
                     {
                         Console.WriteLine($"\nYou guessed the word! It was {chosenWord}. You won!");
                         Console.WriteLine("\nPress any key to quit.");
@@ -62,7 +60,7 @@ namespace HangmanGame
                 }
                 else
                 {
-                    if (guessedLetters.ToString().Contains(letter))
+                    if (guessedLetters.ToString().Contains(letter, StringComparison.OrdinalIgnoreCase))
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("You have already guessed on this letter, try again");
@@ -71,13 +69,13 @@ namespace HangmanGame
 
                     else
                     {
-                        guessedLetters.Append(letter + " ");
-                        if (chosenWord.Contains(letter))
+                        guessedLetters.Append(letter.ToLower() + " ");
+                        if (chosenWord.Contains(letter, StringComparison.OrdinalIgnoreCase))
                         {
-                            var indexes = Regex.Matches(chosenWord, letter).Cast<Match>().Select(m => m.Index).ToList();
+                            var indexes = Regex.Matches(chosenWord, letter, RegexOptions.IgnoreCase).Cast<Match>().Select(m => m.Index).ToList();
                             foreach (var i in indexes)
                             {
-                                word[i] = char.Parse(letter);
+                                word[i] = chosenWord.ElementAt(i);
                             }
 
                             Console.WriteLine("You guessed right.");
@@ -112,7 +110,6 @@ namespace HangmanGame
             Console.WriteLine("\nPress any key to quit.");
             Console.ReadKey();
             Environment.Exit(0);
-
 
         }
     }
